fd57f7be2be4afc8b782acb85bd86b30b8917e75
myname1019 <jhs011019@gmail.com>
2026-02-19 10:34:30 +0900

    ADD : Main 페이지 제작 Modify : 템플릿 기본 폴더 설정

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from Main import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('Board/', include('Board.urls')),
+    path('',views.Main),
 ]
