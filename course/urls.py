3eeab2e7c7dd7e3c1576867b5f6e9531018528f3
hskangfine-star <hskangfine@gmail.com>
2026-02-19 16:26:37 +0900

    course push test

---
+++ course/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'course'
+urlpatterns = [
+    path('', views.CourseList.as_view(), name='course_list'),
+    path('<int:pk>/', views.CourseDetail.as_view(), name='course_detail'),
+    path('create/', views.CourseCreate.as_view(), name='course_create'),
+    path('<int:pk>/update/', views.CourseUpdate.as_view(), name='course_update'),
+    path('<int:pk>/delete/', views.course_delete, name='course_delete'),
+]
--- course/views.py
+++ course/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Course
+from .forms import CourseForm
 
-# Create your views here.
+
+# 1. 강의 목록
+class CourseList(ListView):
+    model = Course
+    template_name = 'course/course_list.html'
+    context_object_name = 'courses'
+
+
+# 2. 강의 상세
+class CourseDetail(DetailView):
+    model = Course
+    template_name = 'course/course_detail.html'
+    context_object_name = 'course'
+
+
+# 3. 강의 생성
+class CourseCreate(CreateView):
+    model = Course
+    form_class = CourseForm
+    template_name = 'course/course_form.html'
+    success_url = reverse_lazy('course:course_list')
+
+
+# 4. 강의 수정
+class CourseUpdate(UpdateView):
+    model = Course
+    form_class = CourseForm
+    template_name = 'course/course_form.html'
+    success_url = reverse_lazy('course:course_list')
+
+
+# 5. 강의 삭제
+def course_delete(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+
+    if request.method == "POST":
+        course.delete()
+        return redirect('course:course_list')
+
+    return redirect('course:course_detail', pk=pk)
+
