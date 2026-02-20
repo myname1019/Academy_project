f572fd0e6c0a45f4e655813db4a1bdf06a62d1c8
myname1019 <jhs011019@gmail.com>
2026-02-20 13:27:05 +0900

    Modify : views 테스트 삭제

--- Board/views.py
+++ Board/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
 def board_list(request):
   return render(request, 'Board/index.html')
-=======
 
-push test by kang
->>>>>>> origin/master
