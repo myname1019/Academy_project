1fbe1fdccd10c3313aec45f058c82f8fa075cbd3
hskangfine-star <hskangfine@gmail.com>
2026-02-23 14:53:11 +0900

    course

--- course/views.py
+++ course/views.py
@@ -9,7 +9,7 @@ class CourseList(ListView):
     model = Course
     template_name = 'course/course_list.html'
     context_object_name = 'courses'
-    paginate_by = 3  # 한 페이지에 10개 강의
+    paginate_by = 12
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
