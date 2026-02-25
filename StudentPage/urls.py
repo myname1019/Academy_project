262e523d43272e5c5e257bcd84ba36b6fea3c235
hskangfine-star <hskangfine@gmail.com>
2026-02-25 13:47:20 +0900

    enroll 구현 성공

--- StudentPage/urls.py
+++ StudentPage/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'StudentPage'
 urlpatterns = [
     path('', views.student_dashboard, name='student_dashboard'),
+    path('enroll/<int:course_id>/', views.enroll_course, name='enroll_course'),
 ]
