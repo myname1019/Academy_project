52f868655d254c93391f40168b77ac3463b1c07d
myname1019 <myname1019@users.noreply.github.com>
2026-05-26 14:35:05 +0900

    Revert portfolio upload commit

--- config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Project package initialization."""
-
-try:
-    import pymysql
-
-    pymysql.install_as_MySQLdb()
-except ImportError:
-    # mysqlclient can be used instead of PyMySQL in production images.
-    pass
